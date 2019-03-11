16b096b585a6dd97ab77adfd0c910accf1a24b53
Chris Frashure <csfrashure@gmail.com>
2019-03-11 18:26:31 -0400

    added Party class, new attributes to PrimaryElection

---
+++ src/Party.java
@@ -0,0 +1,16 @@
+public class Party {
+
+    private final String shortName;
+    private final String name;
+
+
+    public Party(String shortName, String name) {
+        this.shortName = shortName;
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}
--- src/PrimaryElection.java
+++ src/PrimaryElection.java
@@ -3,10 +3,19 @@ import java.util.GregorianCalendar;
 public class PrimaryElection extends Election {
 
     private final int district;
+    private final Party party;
 
-    public PrimaryElection(GregorianCalendar date, Office office, int district) {
+    public PrimaryElection(GregorianCalendar date, Office office, Party party, int district) {
         super(date, office);
         this.district = district;
+        this.party = party;
     }
 
+    public int getDistrict() {
+        return district;
+    }
+
+    public Party getParty() {
+        return party;
+    }
 }

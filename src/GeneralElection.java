47612cf34d3b7dc5626e4726f3ffc37768c6e0f8
Chris Frashure <csfrashure@gmail.com>
2019-03-12 21:38:59 -0400

    initial commit

---
+++ src/GeneralElection.java
@@ -0,0 +1,9 @@
+import java.util.GregorianCalendar;
+
+public class GeneralElection extends Election {
+
+
+    public GeneralElection(GregorianCalendar date, Office office) {
+        super(date, office);
+    }
+}

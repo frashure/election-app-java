5c4694459fb064cf7253ac2ee774e74ed4062d0e
Chris Frashure <csfrashure@gmail.com>
2019-03-29 16:16:58 -0400

    catch SQL integrity constraint violation exception for duplicate entries

--- src/util/ElectionCandidates.java
+++ src/util/ElectionCandidates.java
@@ -3,8 +3,9 @@ package util;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "Duplicates"})
 public final class ElectionCandidates {
 
     public static boolean insert(int candidateId, int electionId) {
@@ -21,7 +22,13 @@ public final class ElectionCandidates {
             return true;
 
 
-        } catch (SQLException e) {
+        }
+        catch (SQLIntegrityConstraintViolationException c) {
+            System.out.println("Duplicate entry");
+            return false;
+        }
+
+        catch (SQLException e) {
             e.printStackTrace();
             return false;
         }

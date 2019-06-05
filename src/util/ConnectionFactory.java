c1ba0462c93d6b3abc5501101e7d8ca6311d68a1
Chris Frashure <csfrashure@gmail.com>
2019-06-05 17:33:17 -0400

    replaced IDE environment variables with InputSteam and Properties object

--- src/util/ConnectionFactory.java
+++ src/util/ConnectionFactory.java
@@ -1,20 +1,31 @@
 package util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactory {
 
-    private static final String USERNAME = System.getenv("DB_USER");
-    private static final String PASS = System.getenv("DB_PASS");
-    private static final String CONN_STRING = System.getenv("DB_URL");
+    private static Properties p = new Properties();
+    private static String username;
+    private static String pass;
+    private static String connString;
 
     public static Connection getConnection() {
         try {
-        return DriverManager.getConnection(CONN_STRING, USERNAME, PASS);
+            InputStream is = new FileInputStream("dataConfig.properties");
+            p.load(is);
+            username = p.getProperty("DB_USER");
+            pass = p.getProperty("DB_PASS");
+            connString = p.getProperty("DB_URL");
+
+            return DriverManager.getConnection(connString, username, pass);
         }
-        catch (SQLException e) {
+        catch (SQLException | IOException e) {
             throw new RuntimeException("Error connecting to database", e);
         }
 

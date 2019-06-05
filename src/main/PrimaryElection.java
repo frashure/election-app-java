d335526c40c3237a28cc5ce548855777d9df0542
Chris Frashure <csfrashure@gmail.com>
2019-06-05 17:32:07 -0400

    removed district attribute and getDistrict; exists in parent object

--- src/main/PrimaryElection.java
+++ src/main/PrimaryElection.java
@@ -4,17 +4,12 @@ import java.util.GregorianCalendar;
 
 public class PrimaryElection extends Election {
 
-    private final int district = 0;
     private final String party;
     private int primaryFor;
 
     public PrimaryElection(int id, GregorianCalendar date, String office, String party, int district, String type) {
         super(id, date, office, type, district);
         this.party = party;
-    }
-
-    public int getDistrict() {
-        return district;
     }
 
     public String getParty() {
